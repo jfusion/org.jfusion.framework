320553c93ddc5df6b42b429fe7f7c8c3cc335990
Morten Hundevad <morten@jfusion.org>
2014-09-01 22:16:36 +0200

    removed functions from testing hook.

--- Tests/Mock/Hook.php
+++ Tests/Mock/Hook.php
@@ -19,96 +19,6 @@ use Joomla\Event\Event;
  */
 class Hook implements LanguageInterface, ApplicationInterface, PluginInterface , PlatformInterface {
 	/**
-	 * Loads a language file for framework
-	 *
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	function onLanguageLoadFramework($event)
-	{
-		// TODO: Implement onLanguageLoadFramework() method.
-	}
-
-	/**
-	 * Loads a language file for plugin
-	 *
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	function onLanguageLoadPlugin($event)
-	{
-		// TODO: Implement onLanguageLoadPlugin() method.
-	}
-
-	/**
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	function onInstallerPluginUninstall($event)
-	{
-		// TODO: Implement onInstallerPluginUninstall() method.
-	}
-
-	/**
-	 * Redirect to another URL.
-	 *
-	 * If the headers have not been sent the redirect will be accomplished using a "301 Moved Permanently"
-	 * or "303 See Other" code in the header pointing to the new location. If the headers have already been
-	 * sent this will be accomplished using a JavaScript statement.
-	 *
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	function onApplicationRedirect($event)
-	{
-		// TODO: Implement onApplicationRedirect() method.
-	}
-
-	/**
-	 * Logout authentication function.
-	 *
-	 * Passed the current user information to the onUserLogout event and reverts the current
-	 * session record back to 'anonymous' parameters.
-	 * If any of the authentication plugins did not successfully complete
-	 * the logout routine then the whole method fails. Any errors raised
-	 * should be done in the plugin as this provides the ability to give
-	 * much more information about why the routine may have failed.
-	 *
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	function onApplicationLogout($event)
-	{
-		// TODO: Implement onApplicationLogout() method.
-	}
-
-	/**
-	 * Login authentication function.
-	 *
-	 * Username and encoded password are passed the onUserLogin event which
-	 * is responsible for the user validation. A successful validation updates
-	 * the current session record with the user's details.
-	 *
-	 * Username and encoded password are sent as credentials (along with other
-	 * possibilities) to each observer (authentication plugin) for user
-	 * validation.  Successful validation will update the current session with
-	 * the user details.
-	 *
-	 * @param Event $event
-	 *
-	 * @return  Event
-	 */
-	public function onApplicationLogin($event)
-	{
-		// TODO: Implement onApplicationLogin() method.
-	}
-
-	/**
 	 * Enqueue a system message.
 	 *
 	 * @param Event $event
@@ -154,6 +64,30 @@ class Hook implements LanguageInterface, ApplicationInterface, PluginInterface ,
 	public function onApplicationGetUser($event)
 	{
 		// TODO: Implement onApplicationGetUser() method.
+	}
+
+	/**
+	 * Loads a language file for framework
+	 *
+	 * @param Event $event
+	 *
+	 * @return  Event
+	 */
+	function onLanguageLoadFramework($event)
+	{
+		// TODO: Implement onLanguageLoadFramework() method.
+	}
+
+	/**
+	 * Loads a language file for plugin
+	 *
+	 * @param Event $event
+	 *
+	 * @return  Event
+	 */
+	function onLanguageLoadPlugin($event)
+	{
+		// TODO: Implement onLanguageLoadPlugin() method.
 	}
 
 	/**
@@ -226,5 +160,15 @@ class Hook implements LanguageInterface, ApplicationInterface, PluginInterface ,
 	function onPlatformRoute($event)
 	{
 		// TODO: Implement onPlatformRoute() method.
+	}
+
+	/**
+	 * @param Event $event
+	 *
+	 * @return  Event
+	 */
+	function onInstallerPluginUninstall($event)
+	{
+		// TODO: Implement onInstallerPluginUninstall() method.
 	}
 }
